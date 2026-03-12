a323207e2e4015cce5ec00e96b66fbe015b6a99d
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-03-12 07:43:48 +0530

    start functions.py

---
+++ functions.py
@@ -0,0 +1,25 @@
+import time 
+
+from dotenv import load_dotenv
+import re
+import streamlit as st
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_chroma import Chroma
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+
+
+
+
+
+
+
+
+
