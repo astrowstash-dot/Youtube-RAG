8c87c7ca0fc2c7edd291cffe195cb4cd289a6c51
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-03-13 07:23:17 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+from streamlit import spinner
+
+from functions import (
+    extract_vid_id, 
+    get_transcript,
+    translate_transcript,
+    get_important_topics,
+    get_notes
+)
+
+
+
